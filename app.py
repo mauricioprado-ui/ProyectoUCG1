57296ed07be31737c8b3de4c6695b72ec7378d75
mauricioprado-ui <mauricio.prado@casagrande.edu.ec>
2026-05-16 10:05:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,10 @@ uploaded_files = st.file_uploader(
 for uploaded_file in uploaded_files:
     df = pd.read_csv(uploaded_file)
     st.write(df)
+monto = st.number_input("Ingrese el monto:", min_value = 0 , max_value = 10000, value=1000)
+interes = st.number_input("Ingrese el interes:",min_value = 0.0 , max_value = 1.0, value=0.10)
+anios = st.number_input("Ingrese el número de años del prestamo:",value=1)
+numero_pagos = st.number_input("Ingrese el número pagos anuales:" , value = 12)
 
+cuota = lf.cuota_prestamo(monto, interes,anios,numero_pagos)
+st.write("Su cuota mensual es: ",cuota)
