468e006188c376b7553b73de534dd72c8fbe651c
mauricioprado-ui <mauricio.prado@casagrande.edu.ec>
2026-05-23 09:39:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import libreria_funciones as lf
+import libreria_clases
 
 st.title("Proyecto final UCG")
 
@@ -8,12 +9,16 @@ st.sidebar.title("Parámetros")
 
 st.sidebar.image("Python_logo.png")
 
+modulo = st.selectbox("Seleccione un modulo", ["Modulo 1", "Modulo 2", "Modulo 3"])
+
 uploaded_files = st.file_uploader(
     "Upload data", accept_multiple_files=True, type="csv"
 )
 for uploaded_file in uploaded_files:
     df = pd.read_csv(uploaded_file)
     st.write(df)
+
+    
 monto = st.number_input("Ingrese el monto:", min_value = 0 , max_value = 10000, value=1000)
 interes = st.number_input("Ingrese el interes:",min_value = 0.0 , max_value = 1.0, value=0.10)
 anios = st.number_input("Ingrese el número de años del prestamo:",value=1)
